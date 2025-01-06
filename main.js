e21a3a8ff84c0175a03e023fbe009b7aeb62b531
Sousi Omine <110832262+SousiOmine@users.noreply.github.com>
2025-01-06 16:35:46 +0900

    定期的に入札履歴を確認して反映するようになった

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const app = express();
 
+
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+
 // API(v1)をまとめるルーター
 const api_v1_router = express.Router();
 app.use("/api/v1", api_v1_router);
-
-app.use("/public", express.static(__dirname + "/public"));
 
 
 const start_date = new Date();
@@ -15,10 +18,10 @@ console.log(start_date);
 // 商品リスト
 let goods = []
 goods.push({ id: "afsdioufsahg", title: "スーパーカー 1円スタート", owner: "藤木", deadline_time: new Date().setMinutes(new Date().getMinutes() + 3),history: [] })
-goods[0].history.push({ from: "藤木", price: "1" })
+goods[0].history.push({ from: "藤木", price: 1 })
 
 goods.push({ id: "fasdfasdfsdagf", title: "大学うさぎの兜焼き", owner: "王泥喜", deadline_time: new Date().setMinutes(new Date().getMinutes() + 5),history: [] })
-goods[1].history.push({ from: "王泥喜", price: "100" })
+goods[1].history.push({ from: "王泥喜", price: 100 })
 
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -40,6 +43,22 @@ api_v1_router.post("/auction/get/:id", (req, res) => {
     const id = req.params.id;
     const good = goods.find(good => good.id === id);
     res.json({ good });
+});
+
+api_v1_router.post("/auction/bid", (req, res) => {
+
+    console.log(req.body);
+    let result = false;
+
+    const target_goods_id = req.body.target_goods_id;
+    const from = req.body.from;
+    const price = Number(req.body.price);
+
+    const good = goods.find(good => good.id === target_goods_id);
+    good.history.push({ from, price });
+
+    result = true;
+    res.json({ result: result });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
--- public/index.js
+++ public/index.js
@@ -5,6 +5,30 @@ const target_goods_zone = document.querySelector("#target_goods_zone");
 
 let target_goods_id = "";
 
+window.addEventListener("DOMContentLoaded", function () {
+	//5秒に1回、入札履歴を取得し更新
+	setInterval(() => {
+		road_bid_history(target_goods_id);
+	}, 5000);
+});
+
+function fetchGoodsFromId(id) {
+	const params = {
+		// URL Encode
+		method: "POST",
+		body: "",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+	};
+	const url = "/api/v1/auction/get/" + id;
+
+	return fetch(url, params).then((response) => {
+		if (!response.ok) throw new Error("check all error");
+		return response.json();
+	});
+}
+
 // 全商品の選択ボタンを出す
 function road_all() {
 	const params = {
@@ -27,58 +51,117 @@ function road_all() {
 			console.log(response.all_goods_ids);
 
 			for (const goods_id of response.all_goods_ids) {
+				fetchGoodsFromId(goods_id).then((response) => {
+					// 商品の取得に成功
+					console.log(response);
 
-				const params = {
-					// URL Encode
-					method: "POST",
-					body: "",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-				};
+					let cover = document.createElement("div");
+					cover.id = "cover_" + response.good.id;
 
-				const url = "/api/v1/auction/get/" + goods_id;
-				fetch(url, params)
-					.then((response) => {
-						if (!response.ok) throw new Error("check all error");
-						return response.json();
-					})
-					.then((response) => {
-						// 商品の取得に成功
-						console.log(response);
-
-						let cover = document.createElement("div");
-						cover.id = "cover_" + response.good.id;
-
-						cover.addEventListener("click", () => {
-							road_detail(response.good.id);
-						});
-
-						let goods_title_area = document.createElement("button");
-						goods_title_area.className = "title";
-						goods_title_area.innerText = response.good.title;
-
-						cover.appendChild(goods_title_area);
-						goods_zone.appendChild(cover);
+					cover.addEventListener("click", () => {
+						road_detail(response.good.id);
 					});
+
+					let goods_title_area = document.createElement("button");
+					goods_title_area.className = "title";
+					goods_title_area.innerText = response.good.title;
+
+					cover.appendChild(goods_title_area);
+					goods_zone.appendChild(cover);
+				});
 			}
 		});
 }
 
 // 単一商品をメイン画面に出す
-function road_detail(id)
-{
+function road_detail(id) {
 	target_goods_id = id;
 
+	fetchGoodsFromId(id).then((response) => {
+		console.log(response);
+
+		//メイン商品ゾーンを初期化
+		target_goods_zone.innerHTML = "";
+
+		const template = `
+				<div>
+					<p>${response.good.title}</p>
+					
+					<div class="price_area">
+						<p>現在の価格</p>
+						<p id="now_price">${response.good.history[response.good.history.length - 1].price}</p>
+						<p>入札履歴<p>
+						<div id="bid_history_zone">
+
+						</div>
+					</div>
+
+					<div class="bid_area">
+						<p>入札する</p>
+						<label>入札価格</label>
+						<input id="bid_input_price" type="number" min="${
+							Number(
+								response.good.history[response.good.history.length - 1].price
+							) + 1
+						}">
+						<button id="bid_button" type="button">入札</button>
+					</div>
+				</div>
+			`;
+		target_goods_zone.innerHTML = template;
+
+		//入札ボタンを押したときの入札イベントを定義
+		const bid_button = document.getElementById("bid_button");
+		bid_button.addEventListener("click", () => {
+			bid(
+				response.good.id,
+				Number(document.getElementById("bid_input_price").value)
+			);
+		});
+
+		road_bid_history(response.good.id);
+		
+	});
+}
+
+// 入札履歴をDOMに追加する
+function road_bid_history(id) {
+	fetchGoodsFromId(id).then((response) => {
+		console.log(response);
+
+		const bid_history_zone = document.getElementById("bid_history_zone");
+		bid_history_zone.innerHTML = "";
+		for (const history of response.good.history) {
+			const his = document.createElement("p");
+			his.innerText = history.from + "さん " + history.price + "円";
+			bid_history_zone.appendChild(his);
+		}
+
+		const now_price = document.getElementById("now_price");
+		now_price.innerText = response.good.history[response.good.history.length - 1].price;
+
+		const bit_input = document.getElementById("bid_input_price");
+		bit_input.value = Number(response.good.history[response.good.history.length - 1].price + 1);
+	});
+}
+
+function bid(target_goods_id, price) {
+	console.log(target_goods_id, price);
 	const params = {
-		// URL Encode
 		method: "POST",
-		body: "",
+		body:
+			"target_goods_id=" +
+			target_goods_id +
+			"&from=" +
+			"webuser" +
+			"&price=" +
+			price,
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded",
 		},
 	};
-	const url = "/api/v1/auction/get/" + id;
+
+	const url = "/api/v1/auction/bid";
 	fetch(url, params)
 		.then((response) => {
 			if (!response.ok) throw new Error("check all error");
@@ -86,29 +169,6 @@ function road_detail(id)
 		})
 		.then((response) => {
 			console.log(response);
-
-			//メイン商品ゾーンを初期化
-			target_goods_zone.innerHTML = "";
-
-			const template = `
-				<div>
-					<p>${response.good.title}</p>
-					
-					<div class="price_area">
-						<p>現在の価格</p>
-						<p>${response.good.history[response.good.history.length - 1].price}</p>
-					</div>
-
-					<div class="bid_area">
-						<p>入札する</p>
-						<label>入札価格</label>
-						<input type="number" min="${Number(response.good.history[response.good.history.length - 1].price) + 1}">
-						<button type="button">入札</button>
-					</div>
-				</div>
-			`;
-			target_goods_zone.innerHTML = template;
-
 		});
 }
 
